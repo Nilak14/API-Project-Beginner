c672af0142d3cc25779403d69935a7c89f66cd56
Nilak <nilakpathak14@gmail.com>
2023-10-31 18:42:03 +0545

    ,

--- Jokes Api/Script/async.js
+++ Jokes Api/Script/async.js
@@ -1,26 +1,29 @@
-const { log } = require("async");
+
 
 const btn = document.querySelector('.btn')
 const content = document.querySelector('.content')
 const img = document.querySelector('.container img')
 const URL = 'https://api.chucknorris.io/jokes/random';
 
-
 btn.addEventListener('click', async () => {
     try {
         const data = await fetch(URL);
         const response = await data.json();
-        const value = response.value;
-        content.textContent = value
-        img.classList.add('shake')
-        setTimeout(() => {
-            img.classList.remove('shake')
-        }, 200);
-    } catch (error) {
-        console.log(error);
+        displayData(response)
+    }
+    catch (error) {
+        content.innerText = error
     }
 
 })
 
 
+function displayData(data) {
+    img.classList.add('shake')
+    setInterval(() => {
+        img.classList.remove('shake')
+    }, 1000)
+    const { value: joke } = data;
+    content.innerText = joke;
+}
 
