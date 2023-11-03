0454aad3f9fead51c6bcffba4d6bbad0df2733f7
Nilak <nilakpathak14@gmail.com>
2023-11-03 19:46:36 +0545

    finished random person

--- Random User/Scripts/app.js
+++ Random User/Scripts/app.js
@@ -10,10 +10,32 @@ const btn = get('.btn');
 const buttons = [...document.querySelectorAll('.icon')]
 
 
+function displayUser(person) {
+    img.src = person.image;
+    value.textContent = person.fullName;
+    title.textContent = 'My name is'
+    buttons.forEach(button => {
+        button.classList.remove('active')
+    })
+    buttons[0].classList.add('active')
+    buttons.forEach(button => {
+        const label = button.dataset.label
+        button.addEventListener('click', () => {
+            button.classList.remove('active')
+            title.textContent = `My ${label} is`
+            value.textContent = person[label];
+            buttons.forEach(button => {
+                button.classList.remove('active')
+            })
+            button.classList.add('active')
+        })
+    })
+
+}
 
 async function showUser() {
     const person = await getUser(URL);
-    console.log(person)
+    displayUser(person)
 }
 
 window.addEventListener('DOMContentLoaded', showUser)
